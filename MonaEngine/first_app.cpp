1996659fa3f09df0ed132049505c3bd106a4f0b1
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-14 22:41:04 +0100

    Tut05 part 02 fin

--- MonaEngine/first_app.cpp
+++ MonaEngine/first_app.cpp
@@ -2,7 +2,7 @@
 
 //std
 #include <stdexcept>
-
+#include <array>
 
 namespace mve {
 
@@ -17,8 +17,10 @@ namespace mve {
 	void FirstApp::run() {
 		while (!mveWindow.shouldClose()) {
 			glfwPollEvents();
-
+			drawFrame();
 		}
+
+		vkDeviceWaitIdle(mveDevice.device());
 	}
 
 	void FirstApp::createPipelineLayout() {
@@ -44,7 +46,67 @@ namespace mve {
 			pipelineConfig);
 	};
 
-	void FirstApp::createCommandBuffers() {}
-	void FirstApp::drawFrame() {}
+	void FirstApp::createCommandBuffers() {
+		
+		commandBuffers.resize(mveSwapChain.imageCount());
+		
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandPool = mveDevice.getCommandPool();
+		allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+		if (vkAllocateCommandBuffers(mveDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
+			throw std::runtime_error("failed to allocate command buffers!");
+		}
+
+		for (int i = 0; i < commandBuffers.size(); i++) {
+			VkCommandBufferBeginInfo beginInfo{};
+			beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+			if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+				throw std::runtime_error("failed to begin recording command buffer!");
+			}
+
+			VkRenderPassBeginInfo renderPassInfo{};
+			renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+			renderPassInfo.renderPass = mveSwapChain.getRenderPass();
+			renderPassInfo.framebuffer = mveSwapChain.getFrameBuffer(i);
+
+			renderPassInfo.renderArea.offset = { 0, 0 };
+			renderPassInfo.renderArea.extent = mveSwapChain.getSwapChainExtent();
+
+			std::array<VkClearValue, 2> clearValues{};
+			clearValues[0].color = { 0.1f, 0.1f, 0.1f, 1.0f };
+			clearValues[1].depthStencil = { 1.0f, 0 };
+			renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+			renderPassInfo.pClearValues = clearValues.data();
+
+			vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+
+			mvePipeline->bind(commandBuffers[i]);
+			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+			vkCmdEndRenderPass(commandBuffers[i]);
+			if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+				throw std::runtime_error("failed to record command buffer!");
+			}
+
+		}
+	}
+
+	void FirstApp::drawFrame() {
+		uint32_t imageIndex;
+		auto result = mveSwapChain.acquireNextImage(&imageIndex);
+		
+		if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+			throw std::runtime_error("failed to acquire swap chain image!");
+		}
+
+		result = mveSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+		if (result != VK_SUCCESS) {
+			throw std::runtime_error("failed to present swap chain image!");
+		}
+	}
 
 } // namespace mve
