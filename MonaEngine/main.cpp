6c567d4134e4e3bcda4dd77be27a56212c97477c
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-23 20:04:48 +0100

    Vector4D repr

--- MonaEngine/main.cpp
+++ MonaEngine/main.cpp
@@ -7,9 +7,15 @@
 #include <stdexcept>
 
 #include "relativity/math/Vector3.hpp"
+#include "relativity/math/Vector4D.hpp"
 
 // For testing relativity math:
 int main() {
+
+	Math::Vector4D vec4D = Math::Vector4D{ 1.0, 5.0, 2.0, 3.0 };
+
+	std::cout << vec4D << '\n';
+
 	return EXIT_SUCCESS;
 }
 
--- MonaEngine/relativity/math/Vector4D.cpp
+++ MonaEngine/relativity/math/Vector4D.cpp
@@ -2,4 +2,11 @@
 
 namespace Math {
 
+
+
+
+	std::ostream& operator<<(std::ostream& os, const Vector4D& v) {
+		os << "Vector4{" << v.t << ", " << v.x << ", " << v.y << ", " << v.z << "}" << '\n';
+		return os;
+	}
 }
