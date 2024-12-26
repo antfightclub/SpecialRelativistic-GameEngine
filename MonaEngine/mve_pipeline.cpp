f1d473087ec19f6148f1be1198040c59a9386132
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-26 17:26:15 +0100

    naive change of pipeline primitive topology

--- MonaEngine/mve_pipeline.cpp
+++ MonaEngine/mve_pipeline.cpp
@@ -210,10 +210,9 @@ MvePipeline::~MvePipeline() {
 	}
 
 	void MvePipeline::wireframePipelineConfigInfo(PipelineConfigInfo& configInfo) {
-
 		// Input Assembly state create info default config
 		configInfo.inputAssemblyInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-		configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
+		configInfo.inputAssemblyInfo.topology = VK_PRIMITIVE_TOPOLOGY_LINE_LIST;
 		configInfo.inputAssemblyInfo.primitiveRestartEnable = VK_FALSE;
 
 		// ViewportInfo
