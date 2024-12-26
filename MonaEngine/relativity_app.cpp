0504f20d91388fd1ba22274f27d9333fd1ffa018
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-26 17:09:50 +0100

    relativityapp LoadGamObjects load lattice

--- MonaEngine/relativity_app.cpp
+++ MonaEngine/relativity_app.cpp
@@ -4,6 +4,7 @@
 #include "mve_camera.hpp"
 #include "systems/lattice_wireframe_system.hpp"
 #include "mve_buffer.hpp"
+#include "lattice.hpp"
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
@@ -37,5 +38,15 @@ namespace mve {
 
 	void RelativityApp::loadGameObjects() {
 		// generate lattice
+		Lattice lattice;
+		lattice.makeLattice(30.0, 100.0, 1.0);
+		auto vertices = lattice.getVertices();
+		auto indices = lattice.getIndices();
+		std::shared_ptr<MveModel> mveModel = MveModel::createModelFromStdVectors(mveDevice, vertices, indices);
+		auto latticeGameObject = MveGameObject::createGameObject();
+		latticeGameObject.model = mveModel;
+		latticeGameObject.transform.translation = { 0.f,0.f,0.f };
+		latticeGameObject.transform.scale = glm::vec3{ 1.f, 1.f, 1.f };
+		gameObjects.emplace(latticeGameObject.getId(), std::move(latticeGameObject));
 	}
 }
