ba8928c4f75bf7f27e6ce38a1c9d645492a1e761
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-26 17:09:08 +0100

    Create Lattice algorithm

--- MonaEngine/lattice.cpp
+++ MonaEngine/lattice.cpp
@@ -1,11 +1,72 @@
 #include "lattice.hpp"
+#include "mve_device.hpp"
 
 #include <vector>
+#include <stdexcept>
 
 namespace mve {
 
+	std::vector<glm::vec3> Lattice::getVertices() {
+		if (this->vertices.empty()) {
+			throw std::runtime_error("Failed to get vertices since vertex vector is empty!");
+		}
+		return this->vertices;
+	}
+	std::vector<uint32_t> Lattice::getIndices() {
+		if (this->indices.empty()) {
+			throw std::runtime_error("Failed to get indices since index vector is empty!");
+		}
+		return this->indices;
+	}
+
 	void Lattice::makeLattice(double N, double L, double scale) {
-				
+		//std::shared_ptr<MveModel> mveModel = MveModel::MveModel(mveDevice);
+		int n = 2 * N + 1;
+		int xx, yy, zz;
+		for (int i = -N; i < N + 1; i++) {
+			xx = (i + 0.5) * (L / N);
+			for (int j = -N; j < N + 1; j++) {
+				yy = (j + 0.5) * (L / N);
+				for (int k = -N*c; k < N*c + 1; k++) {
+					zz = (((k + 0.5) * c) * L) / (N * c);
+					add(xx, yy, zz, k);
+				}
+
+			}
+		}
+
+		for (int i = -N; i < N + 1; i++) {
+			xx = (i + 0.5) * (L / N);
+			for (int j = -N; j < N + 1; j++) {
+				zz = (j + 0.5) * (L / N);
+				for (int k = -N * c; k < N * c + 1; k++) {
+					yy = (((k + 0.5) * c) * L) / (N * c);
+					add(xx, yy, zz, k);
+				}
+
+			}
+		}
+
+		for (int i = -N; i < N + 1; i++) {
+			zz = (i + 0.5) * (L / N);
+			for (int j = -N; j < N + 1; j++) {
+				yy = (j + 0.5) * (L / N);
+				for (int k = -N * c; k < N * c + 1; k++) {
+					xx = (((k + 0.5) * c) * L) / (N * c);
+					add(xx, yy, zz, k);
+				}
+
+			}
+		}
+
+	}
+
+	void Lattice::add(double xx, double yy, double zz, double a) {
+		vertices.push_back(glm::vec3{ xx, yy, zz });
+		if (a < N * c) {
+			indices.push_back(static_cast<uint32_t>(vertices.size()) / 3 - 1);
+			indices.push_back(static_cast<uint32_t>(vertices.size()) / 3);
+		}
 	}
 
 
--- MonaEngine/lattice.hpp
+++ MonaEngine/lattice.hpp
@@ -5,17 +5,27 @@
 namespace mve {
 	class Lattice {
 	public:
+
+		
 		
 		//Lattice();
 
 		//~Lattice();
-
+		
 		void makeLattice(double N, double L, double scale);
+		
+		void add(double xx, double yy, double zz, double a);
+		
+		std::vector<glm::vec3> getVertices();
+		std::vector<uint32_t> getIndices();
 	
 	private:
-		double range = 100.0;
-		double div = 30;
-		int lineWidth = 1;
-		int innerDiv = 5;
+		double L = 100.0;	// range 
+		double N = 30.0;	// div
+		//int lineWidth = 1;
+		int c = 5;	// innerDiv
+
+		std::vector<glm::vec3> vertices{};
+		std::vector<uint32_t> indices{};
 	};
 }
