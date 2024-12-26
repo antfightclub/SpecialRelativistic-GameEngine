867ddbfc400faf979a866326b75cb56fae202334
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-26 23:43:09 +0100

    update main.cpp to reflect new app

--- MonaEngine/main.cpp
+++ MonaEngine/main.cpp
@@ -1,14 +1,16 @@
 
-#include "first_app.hpp"
+//#include "first_app.hpp"
+#include "relativity_app.hpp"
 
 //std
 #include <cstdlib>
 #include <iostream>
 #include <stdexcept>
 
-#include "relativity/math/Vector3.hpp"
-#include "relativity/math/Vector4D.hpp"
-#include "relativity/math/Matrix44.hpp"
+
+//#include "relativity/math/Vector3.hpp"
+//#include "relativity/math/Vector4D.hpp"
+//#include "relativity/math/Matrix44.hpp"
 
 // For testing relativity math:
 //int main() {
@@ -28,9 +30,27 @@
 
 // VULKAN APP MAIN:
 
+//int main() {
+//	mve::FirstApp app{};
+//	try {
+//		app.run();
+//	}
+//	catch (const std::exception& e) {
+//		std::cerr << e.what() << '\n';
+//		return EXIT_FAILURE;
+//	}
+//
+//	return EXIT_SUCCESS;
+//	
+//}
+
 int main() {
-	mve::FirstApp app{};
+	std::cout << "Main begun!" << '\n';
+
+	mve::RelativityApp app{};
+	std::cout << "app initialized! " << '\n';
 	try {
+		std::cout << "trying to run." << '\n';
 		app.run();
 	}
 	catch (const std::exception& e) {
@@ -39,5 +59,4 @@ int main() {
 	}
 
 	return EXIT_SUCCESS;
-	
 }
