6e61f33a602596cb20f021e045e56a83d96d0737
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-26 19:53:55 +0100

    Implement relativity app run function

--- MonaEngine/relativity_app.cpp
+++ MonaEngine/relativity_app.cpp
@@ -31,27 +31,125 @@ namespace mve {
 	RelativityApp::~RelativityApp() {}
 	
 	void RelativityApp::run() {
-		// uboBuffers
 
-		// wireframeUboBuffer
+		// (I Think) this is buffers for each frame in flight
+		std::vector<std::unique_ptr<MveBuffer>> globalUboBuffers(MveSwapChain::MAX_FRAMES_IN_FLIGHT);
+		for (int i = 0; i < globalUboBuffers.size(); i++) {
+			globalUboBuffers[i] = std::make_unique<MveBuffer>(
+				mveDevice,
+				sizeof(GlobalUbo),
+				1, 
+				VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+			);
+			globalUboBuffers[i]->map();
+		}
 
-		// map global buffer
+		MveBuffer globalUboBuffer{
+			mveDevice,
+			sizeof(GlobalUbo),
+			MveSwapChain::MAX_FRAMES_IN_FLIGHT,
+			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+			mveDevice.properties.limits.minUniformBufferOffsetAlignment,
+		};
+		
+		std::vector<std::unique_ptr<MveBuffer>> latticeUboBuffers(MveSwapChain::MAX_FRAMES_IN_FLIGHT);
+		for (int i = 0; i < latticeUboBuffers.size(); i++) {
+			latticeUboBuffers[i] = std::make_unique<MveBuffer>(
+				mveDevice,
+				sizeof(LatticeUbo),
+				1,
+				VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT
+				);
+			latticeUboBuffers[i]->map();
+		}
 
-		// gloablSetLayout
+		MveBuffer latticeUboBuffer{
+			mveDevice,
+			sizeof(LatticeUbo),
+			MveSwapChain::MAX_FRAMES_IN_FLIGHT,
+			VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+			VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT,
+			mveDevice.properties.limits.minUniformBufferOffsetAlignment,
+		};
 
-		// descriptorsets
+		globalUboBuffer.map();
+		latticeUboBuffer.map();
 
-		// add render system (lattice this time)
+		auto descriptorSetLayout = MveDescriptorSetLayout::Builder(mveDevice)
+			.addBinding(0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT) // Global Ubo 
+			.addBinding(1, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_VERTEX_BIT)								 // Lattice Ubo
+			.build();
+		
+		std::vector<VkDescriptorSet> globalDescriptorSets(MveSwapChain::MAX_FRAMES_IN_FLIGHT);
+		for (int i = 0; i < globalDescriptorSets.size(); i++) {
+			auto globalBufferInfo = globalUboBuffers[i]->descriptorInfo();
+			auto latticeBufferInfo = latticeUboBuffers[i]->descriptorInfo();
+			MveDescriptorWriter(*descriptorSetLayout, *globalPool)
+				.writeBuffer(0, &globalBufferInfo)
+				.writeBuffer(1, &latticeBufferInfo)
+				.build(globalDescriptorSets[i]);
+		}
 
-		// init camera
+		LatticeWireframeSystem latticeRenderSystem{ mveDevice, mveRenderer.getSwapChainRenderPass(), descriptorSetLayout->getDescriptorSetLayout() };
+		MveCamera camera{};
+		camera.setViewDirection(glm::vec3(0.f), glm::vec3(0.5f, 0.f, 1.f));
+		camera.setViewTarget(glm::vec3(-1.f, -2.f, -2.f), glm::vec3(0.f, 0.f, 2.5f));
 
-		// create viewerobject
-		// create camera controller
-		// current time
+		auto viewerObject = MveGameObject::createGameObject();
+		viewerObject.transform.translation.z = -2.5f;
+		KeyboardMovementController cameraController{};
 
-		// while !mveWindow.shouldClose() (update loop)
-		//...etc
+		auto currentTime = std::chrono::high_resolution_clock::now();
+	
+		while (!mveWindow.shouldClose()) {
+			glfwPollEvents();
 
+			auto newTime = std::chrono::high_resolution_clock::now();
+			float frameTime = std::chrono::duration<float, std::chrono::seconds::period>(newTime - currentTime).count();
+			
+			cameraController.moveInPlaneXZ(mveWindow.getGLFWwindow(), frameTime, viewerObject);
+			camera.setViewYXZ(viewerObject.transform.translation, viewerObject.transform.rotation);
+
+			float aspect = mveRenderer.getAspectRatio();
+
+			camera.setPerspectiveProjection(glm::radians(50.f), aspect, 0.1f, 1000.0f);
+
+			if (auto commandBuffer = mveRenderer.beginFrame()) {
+				int frameIndex = mveRenderer.getFrameIndex();
+				FrameInfo frameInfo{
+					frameIndex,
+					frameTime,
+					commandBuffer,
+					camera,
+					globalDescriptorSets[frameIndex],
+					gameObjects
+				};
+
+				// ***** update
+
+				// update global ubo buffer
+				GlobalUbo globalUbo{};
+				globalUbo.projection = camera.getProjection();
+				globalUbo.view = camera.getView();
+				globalUbo.inverseView = camera.getInverseView();
+				globalUboBuffers[frameIndex]->writeToBuffer(&globalUbo);
+				globalUboBuffers[frameIndex]->flush();
+				
+				// TODO: update lattice ubo buffer
+				LatticeUbo latticeUbo{};
+				
+				// Render
+
+				// order matters (if semitransparency is involved)
+				latticeRenderSystem.renderWireframe(frameInfo);
+
+				mveRenderer.endSwapChainRenderPass(commandBuffer);
+				mveRenderer.endFrame();				
+			}
+		}
 
 
 		vkDeviceWaitIdle(mveDevice.device());
