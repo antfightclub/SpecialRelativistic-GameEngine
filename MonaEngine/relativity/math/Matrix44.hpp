ed9558946d2b1086cf65a803b9ae458572313558
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-29 19:57:05 +0100

    Matrix44 constructor from 16 floats

--- MonaEngine/relativity/math/Matrix44.hpp
+++ MonaEngine/relativity/math/Matrix44.hpp
@@ -36,6 +36,15 @@ namespace Math {
 			m20{ 0.0 },  m21{ 0.0 }, m22{ diag }, m23{ 0.0 },
 			m30{ 0.0 },  m31{ 0.0 }, m32{ 0.0 },  m33{ diag } {};
 
+		Matrix44(double m00, double m01, double m02, double m03,
+				 double m10, double m11, double m12, double m13,
+				 double m20, double m21, double m22, double m23,
+				 double m30, double m31, double m32, double m33) :
+			m00{ m00 }, m01{ m01 }, m02{ m02 }, m03{ m03 },
+			m10{ m10 }, m11{ m11 }, m12{ m12 }, m13{ m13 },
+			m20{ m20 }, m21{ m21 }, m22{ m22 }, m23{ m23 },
+			m30{ m30 }, m31{ m31 }, m32{ m32 }, m33{ m33 } {};
+
 		// // ********** String representation **********
 		friend std::ostream& operator<<(std::ostream& os, const Matrix44& mat);
 
