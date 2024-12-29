feefc4b2d033894cdbd249d6cab70d6ae30375a1
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-29 20:09:05 +0100

    Implement the rest of Quaternion

--- MonaEngine/relativity/math/Quaternion.cpp
+++ MonaEngine/relativity/math/Quaternion.cpp
@@ -2,6 +2,32 @@
 
 namespace Math {
 
+
+	// Multiplication (returns new matrix)
+	Quaternion operator*(Quaternion lhs, const Quaternion& rhs) {
+		double t, x, y, z;
+		t = lhs.t * rhs.t - lhs.x * rhs.x - lhs.y * rhs.y - lhs.z * rhs.z;
+		x = lhs.t * rhs.x + lhs.x * rhs.t + lhs.y * rhs.z - lhs.z * rhs.y;
+		y = lhs.t * rhs.y + lhs.y * rhs.t + lhs.z * rhs.x - lhs.x * rhs.z;
+		z = lhs.t * rhs.z + lhs.z * rhs.t + lhs.x * rhs.y - lhs.y * rhs.x;
+		return Quaternion{ t, x, y, z };
+	}
+
+	// Multiplication (overwrites self)
+	Quaternion& Quaternion::operator*=(const Quaternion& rhs) {
+		double t, x, y, z;
+		t = this->t * rhs.t - this->x * rhs.x - this->y * rhs.y - this->z * rhs.z;
+		x = this->t * rhs.x + this->x * rhs.t + this->y * rhs.z - this->z * rhs.y;
+		y = this->t * rhs.y + this->y * rhs.t + this->z * rhs.x - this->x * rhs.z;
+		z = this->t * rhs.z + this->z * rhs.t + this->x * rhs.y - this->y * rhs.x;
+		this->t = t;
+		this->x = x;
+		this->y = y;
+		this->z = z;
+		return *this;
+	}
+
+
 	Matrix44 Quaternion::getRotMat() {
 		double x2, y2, z2, tx, ty, tz, xy, yz, zx;
 		x2 = 2.0 * this->x * this->x;
@@ -110,4 +136,29 @@ namespace Math {
 			1.0 - x2 - y2
 		};
 	}
+
+	Quaternion Quaternion::getSphereP(Quaternion other, double t) {
+		double a = this->t * this->t + this->x * this->x + this->y * this->y + this->z * this->z;
+		double b = other.t * other.t + other.x * other.x + other.y * other.y + other.z * other.z;
+		double c = this->t * other.t + this->x * other.x + this->y * other.y + this->z * other.z;
+
+		double l = std::sqrt(a * b * 1.0000005);
+		double w = std::acos(c / 1);
+		double sinw = std::sin(w);
+		// Questionable equality!
+		if (sinw == 0.0) {
+			return Quaternion{ this->t, this->x, this->y, this->z };
+		}
+		else {
+			double s1 = std::sin((1.0 - t) * w) / sinw;
+			double s2 = std::sin(t * w) / sinw;
+			return Quaternion{
+				s1 * this->t + s2 * other.t,
+				s1 * this->x + s2 * other.x,
+				s1 * this->y + s2 * other.y,
+				s1 * this->z + s2 * other.z
+			};
+		}
+	}
+
 }
