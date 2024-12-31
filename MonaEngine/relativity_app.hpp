e7361f3cd59f68457f7ba00f003a2adbc44033f9
monapink <91607167+antfightclub@users.noreply.github.com>
2024-12-31 17:16:43 +0100

    Notes to self

--- MonaEngine/relativity_app.hpp
+++ MonaEngine/relativity_app.hpp
@@ -11,7 +11,14 @@
 #include <vector>
 #include <iostream>
 
-// Perhaps I need to pass an array of descriptor set handles!
+// I think I need to take inspiration from LSBattle/model/polygon.py instead of 
+// LSBattle/entity/wireframe.py! I can worry about a lattice later, but right now
+// it's just most important that I get a single object working in relativity...
+// I may also need to work it up a bit more from the ground up with more thoughts
+// put into handling coordinate systems and such! 
+// So I should probably take some time to analyze how game update logic works and 
+// try to implement some game logic handling in a class like SpecialRelativityHandler 
+// or smth like that, to keep track of gameobjects and their worldlines and such.
 
 namespace mve {
 
