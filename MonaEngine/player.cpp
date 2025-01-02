12af5df265fb33e624bbeb822e1da696995ce77a
monapink <91607167+antfightclub@users.noreply.github.com>
2025-01-03 00:21:28 +0100

    Player input migrate to mvewindow k_state + debug

--- MonaEngine/player.cpp
+++ MonaEngine/player.cpp
@@ -1,7 +1,26 @@
 #include "player.hpp"
 
-namespace SRGame {
+namespace mve {
+
+	Player::Player(MveWindow& mveWindow, MveGameObject& gameObject, Math::Vector4D pos, Math::EntityState firstState) : mveWindow{ mveWindow }, P{ Math::PhaseSpace{pos, Math::Vector4D(1.0, 0.0, 0.0, 0.0)} }, worldline{ gameObject.getId(), pos, firstState } {
+		init(mveWindow.getGLFWwindow(), gameObject);
+	}
+
+
 	void Player::Action(GLFWwindow* window, double deltaTime) {
+		//updateKeyMap(window);
+
+				// Print keymap state
+		std::cout << "***** EVIL KEYMAP STATE *****\n"
+			<< "k_accel " << mveWindow.k_state.k_accel << '\n'
+			<< "k_accel_priority " << mveWindow.k_state.k_accel_priority << '\n'
+			<< "k_turn_right " << mveWindow.k_state.k_turn_right << '\n'
+			<< "k_turn_left " << mveWindow.k_state.k_turn_left << '\n'
+			<< "k_turn_up " << mveWindow.k_state.k_turn_up << '\n'
+			<< "k_turn_down " << mveWindow.k_state.k_turn_down << '\n'
+			<< "k_turn_priority_1 " << mveWindow.k_state.k_turn_priority_1 << '\n'
+			<< "k_turn_priority_2 " << mveWindow.k_state.k_turn_priority_2 << '\n';
+
 		changeDirection(window, deltaTime);
 
 		Math::Vector4D acceleration = this->getAcceleration(window, deltaTime);
@@ -12,43 +31,113 @@ namespace SRGame {
 	}
 
 	void Player::changeDirection(GLFWwindow* window, double deltaTime) {
-		if (glfwGetKey(window, keyboardController.keys.turn_right) == GLFW_PRESS) {
-			turnSpeed1 += this->turnAcceleration * deltaTime;
+
+
+		// Evil
+		if (mveWindow.k_state.k_turn_priority_1 == 0) {
+			if (mveWindow.k_state.k_turn_right) {
+				this->turnSpeed1 += this->turnAcceleration * deltaTime;
+			}
+			else if (mveWindow.k_state.k_turn_left) {
+				this->turnSpeed1 -= this->turnAcceleration * deltaTime;
+			}
 		}
-		if (glfwGetKey(window, keyboardController.keys.turn_left) == GLFW_PRESS) {
-			turnSpeed1 -= this->turnAcceleration * deltaTime;
+		else {
+			if (mveWindow.k_state.k_turn_left) {
+				this->turnSpeed1 -= this->turnAcceleration * deltaTime;
+			}
+			else if (mveWindow.k_state.k_turn_right) {
+				this->turnSpeed1 += this->turnAcceleration * deltaTime;
+			}
 		}
 
-		if (glfwGetKey(window, keyboardController.keys.turn_up) == GLFW_PRESS) {
-			turnSpeed2 -= this->turnAcceleration * deltaTime;
+		if (mveWindow.k_state.k_turn_priority_2 == 0) {
+			if (mveWindow.k_state.k_turn_up) {
+				this->turnSpeed2 -= this->turnAcceleration * deltaTime;
+			}
+			else if (mveWindow.k_state.k_turn_down) {
+				this->turnSpeed2 += this->turnAcceleration * deltaTime;
+			}
 		}
-		if (glfwGetKey(window, keyboardController.keys.turn_down) == GLFW_PRESS) {
-			turnSpeed2 += this->turnAcceleration * deltaTime;
+		else {
+			if (mveWindow.k_state.k_turn_down) {
+				this->turnSpeed2 += this->turnAcceleration * deltaTime;
+			}
+			else if (mveWindow.k_state.k_turn_up) {
+				this->turnSpeed2 -= this->turnAcceleration * deltaTime;
+			}
 		}
 
+		
 		Math::Matrix44 mat = this->quaternion.getRotMat();
-		this->quaternion *= Math::Quaternion{ this->turnSpeed1 * deltaTime, mat.getUp() };
-		this->quaternion *= Math::Quaternion{ this->turnSpeed2 * deltaTime, mat.getRight() };
+		std::cout << "*********** QUAT DEBUG ********\n";
+		double length = std::sqrt(this->quaternion.t* this->quaternion.t + this->quaternion.x* this->quaternion.x + this->quaternion.y * this->quaternion.y + this->quaternion.z* this->quaternion.z);
+		std::cout << "Player::quaternion length = " << length << "\n";
+		std::cout << "Player::changeDirection rotmat from quat:" << mat << '\n';
+
+		std::cout << "mat.getUp().getNormalize() " << mat.getUp().getNormalize() << '\n';
+		std::cout << "mat.getRight().getNormalize() " << mat.getRight().getNormalize() << '\n';
+
+		Math::Vector3 vec = mat.getUp().getNormalize();
+		
+		double dotUpRight = vec.dot(mat.getRight().getNormalize());
+		double dotUpForward = vec.dot(mat.getForward().getNormalize());
+
+
+		std::cout << "dot between up and right " << dotUpRight << "\n";
+		std::cout << "dot between up and forward " << dotUpForward << "\n";
+		
+
+		std::cout << "*********** END QUAT DEBUG *********\n";
+
+		//Math::Quaternion quatcopy = this->quaternion;
+		/*glm::mat4 mat = glm::toMat4(this->quaternion);
+		glm::quat copy = this->quaternion;*/
+
+		this->quaternion *= Math::Quaternion{ this->turnSpeed1 * deltaTime, mat.getUp().getNormalize()};
+		this->quaternion.normalize();
+		this->quaternion *= Math::Quaternion{ this->turnSpeed2 * deltaTime, mat.getRight().getNormalize()};
+		this->quaternion.normalize();
+
 		this->turnSpeed1 -= this->turnResistivity * this->turnSpeed1 * deltaTime;
 		this->turnSpeed2 -= this->turnResistivity * this->turnSpeed2 * deltaTime;
 	}
 
 	Math::Vector4D Player::getAcceleration(GLFWwindow* window, double deltaTime) {
+		int accel = mveWindow.k_state.k_accel;
 		Math::Vector4D ac{};
 		Math::Matrix44 rotationMatrix = this->quaternion.getRotMat();
 
-		if (glfwGetKey(window, keyboardController.keys.accel_forward) == GLFW_PRESS) {
+		
+
+		// Evil 
+		if (accel & 1) {
 			ac = -Math::Vector4D{ 0.0, rotationMatrix.getForward() };
 		}
-		if (glfwGetKey(window, keyboardController.keys.accel_back) == GLFW_PRESS) {
+		else if (accel & 2) {
 			ac = Math::Vector4D{ 0.0, rotationMatrix.getForward() };
 		}
-		if (glfwGetKey(window, keyboardController.keys.accel_left) == GLFW_PRESS) {
-			ac += Math::Vector4D{ 0.0, rotationMatrix.getRight() };
+		else {
+			ac = Math::Vector4D{};
 		}
-		if (glfwGetKey(window, keyboardController.keys.accel_right) == GLFW_PRESS) {
-			ac -= Math::Vector4D{ 0.0, rotationMatrix.getRight() };
+
+		if (mveWindow.k_state.k_accel_priority == 0) {
+			if (accel & 4) {
+				ac += Math::Vector4D{ 0.0, rotationMatrix.getRight() };
+			}
+			else if (accel & 8) {
+				ac -= Math::Vector4D{ 0.0, rotationMatrix.getRight() };
+			}
 		}
+		else {
+			if (accel & 8) {
+				ac -= Math::Vector4D{ 0.0, rotationMatrix.getRight() };
+			}
+			else if (accel & 4) {
+				ac += Math::Vector4D{ 0.0, rotationMatrix.getRight() };
+			}
+		}
+
 		ac.normalize();
 		return ac;
 	}
--- MonaEngine/player.hpp
+++ MonaEngine/player.hpp
@@ -7,37 +7,38 @@
 #include "relativity/math/Quaternion.hpp"
 #include "relativity/math/PhaseSpace.hpp"
 #include "relativity/math/WorldLine.hpp"
-#include "keyboard_movement_controller.hpp"
+
 
 #include "mve_game_object.hpp"
 #include "mve_model.hpp"
 
-namespace SRGame {
+#include <iostream>
+
+namespace mve {
 	// Player entity
 
 	struct PlayerState {
 		// Empty for now. For example, guns. Maybe.
 	};
+	
 
 
 	class Player {
 	public:
 
-		Player(mve::MveGameObject& gameObject, Math::Vector4D pos, Math::EntityState firstState) : P{ Math::PhaseSpace{pos, Math::Vector4D(1.0, 0.0, 0.0, 0.0)} }, worldline{ gameObject.getId(), pos, firstState } {
-			init(gameObject);
-		}
+		Player(MveWindow& mveWindow, MveGameObject& gameObject, Math::Vector4D pos, Math::EntityState firstState);
 
 		Math::PhaseSpace P;
-		Math::Quaternion quaternion{1.0, 0.0, 0.0, 0.0};
+		Math::Quaternion quaternion{ 1.0, 0.0, 0.0, 0.0 };
+		//glm::quat quaternion{ 1.0, 0.0, 0.0, 0.0 };
 		double time = 0.0;
 		Math::WorldLine worldline;
-		mve::KeyboardInputController keyboardController{};
-		
+
 		void Action(GLFWwindow* window, double deltaTime);
 
-
 	private:
-		mve::MveGameObject::id_t id; 
+		MveWindow& mveWindow;
+		MveGameObject::id_t id;
 		double acceleration = 1.8;
 		double resistivity = 2.0;
 		double turnAcceleration = 9.0;
@@ -45,20 +46,14 @@ namespace SRGame {
 		double turnSpeed1 = 0.0;
 		double turnSpeed2 = 0.0;
 
-		// Accel and turn priorities??
-
-		
-
-		void init(mve::MveGameObject& gameObject){
-			this->id = gameObject.getId();
-		}
+		void init(GLFWwindow* window, mve::MveGameObject& gameObject) {	this->id = gameObject.getId(); }
 
 		Math::Vector4D getAcceleration(GLFWwindow* window, double deltaTime);
 		// calc repulsion
-		
+
 		void changeDirection(GLFWwindow* window, double deltaTime);
 		//getviscous
 
-		
 	};
+
 }
