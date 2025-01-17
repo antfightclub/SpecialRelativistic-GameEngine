825b865e7c5ee62c53fa0ca5144ad4dcd9a2e2ab
monapink <91607167+antfightclub@users.noreply.github.com>
2025-01-17 14:58:35 +0100

    fix validation error

--- MonaEngine/systems/sr_render_system.cpp
+++ MonaEngine/systems/sr_render_system.cpp
@@ -23,7 +23,7 @@ namespace mve {
 
 	void SRRenderSystem::createPipelineLayout(VkDescriptorSetLayout globalDescriptorSetLayout) {
 		VkPushConstantRange pushConstantRange{};
-		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+		pushConstantRange.stageFlags = VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT;
 		pushConstantRange.offset = 0;
 		pushConstantRange.size = sizeof(PushConstantData);
 		
