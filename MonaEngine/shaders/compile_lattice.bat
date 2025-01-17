e925fb67d996015171d2924e9b1bd890645e1f16
monapink <91607167+antfightclub@users.noreply.github.com>
2025-01-17 14:16:48 +0100

    add new shaders and update compile batch script

--- MonaEngine/shaders/compile_lattice.bat
+++ MonaEngine/shaders/compile_lattice.bat
@@ -7,4 +7,13 @@ copy /b  lattice_shader.vert.spv ..\..\x64\Release\shaders\lattice_shader.vert.s
 copy /b  lattice_shader.frag.spv ..\..\x64\Debug\shaders\lattice_shader.frag.spv
 copy /b  lattice_shader.frag.spv ..\..\x64\Release\shaders\lattice_shader.frag.spv
 
+D:\VulkanSDK\1.3.296.0\Bin\glslc.exe polygon_shader.vert -o polygon_shader.vert.spv
+D:\VulkanSDK\1.3.296.0\Bin\glslc.exe polygon_shader.frag -o polygon_shader.frag.spv
+
+copy /b  polygon_shader.vert.spv ..\..\x64\Debug\shaders\polygon_shader.vert.spv
+copy /b  polygon_shader.vert.spv ..\..\x64\Release\shaders\polygon_shader.vert.spv
+		 
+copy /b  polygon_shader.frag.spv ..\..\x64\Debug\shaders\polygon_shader.frag.spv
+copy /b  polygon_shader.frag.spv ..\..\x64\Release\shaders\polygon_shader.frag.spv
+
 pause
--- MonaEngine/shaders/polygon_shader.vert
+++ MonaEngine/shaders/polygon_shader.vert
@@ -1,3 +1,4 @@
+#version 450
 // vec4 coor
 // mat4 Lorentz
 // mat4 Lorentz_p2e
@@ -6,3 +7,7 @@
 // vec4 xp			player X in background frame
 
 // https://github.com/sogebu/LSBattle/blob/fbde754c5aee3250e42ac3f9ab5a5e4a4379ce92/model/polygon.py
+
+void main() {
+
+}
