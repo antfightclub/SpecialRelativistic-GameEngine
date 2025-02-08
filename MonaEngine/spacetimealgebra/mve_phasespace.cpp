95681465031024be4e08eb118b1c157ccaa054c6
monapink <91607167+antfightclub@users.noreply.github.com>
2025-02-08 20:21:05 +0100

    test of implementation of mvephasespace transform

--- MonaEngine/spacetimealgebra/mve_phasespace.cpp
+++ MonaEngine/spacetimealgebra/mve_phasespace.cpp
@@ -23,10 +23,6 @@ namespace mve {
 		mv Bhat20 = g2 * g0;
 		mv Bhat30 = g3 * g0;
 
-		std::cout << "bhat10 squared    : " << (Bhat10 * Bhat10).toString() << std::endl;
-		std::cout << "bhat20 squared    : " << (Bhat20 * Bhat20).toString() << std::endl;
-		std::cout << "bhat30 squared    : " << (Bhat30 * Bhat30).toString() << std::endl << std::endl;
-
 		double rapidityx = 0.5 * (log((1.0 + betax) / (1.0 - betax)));
 		double rapidityy = 0.5 * (log((1.0 + betay) / (1.0 - betay)));
 		double rapidityz = 0.5 * (log((1.0 + betaz) / (1.0 - betaz)));
@@ -39,13 +35,59 @@ namespace mve {
 		mv accely = accel.get_g2() * g2;
 		mv accelz = accel.get_g3() * g3;
 
+		accelx *= ds;
+		accely *= ds;
+		accelz *= ds;
+
+		std::cout << "betax             : " << betax << std::endl;
+		std::cout << "betay             : " << betay << std::endl;
+		std::cout << "betaz             : " << betaz << std::endl;
+
+		std::cout << "rapidityx         : " << rapidityx << std::endl;
+		std::cout << "rapidityy         : " << rapidityy << std::endl;
+		std::cout << "rapidityz         : " << rapidityz << std::endl;
+
+		std::cout << "Lx                : " << Lx.toString() << std::endl;
+		std::cout << "Ly                : " << Ly.toString() << std::endl;
+		std::cout << "Lz                : " << Lz.toString() << std::endl;
+
 		accelx = (~Lx) * accelx * Lx;
 		accely = (~Ly) * accely * Ly;
 		accelz = (~Lz) * accelz * Lz;
 
-		velocity.set_g1(velocity.get_g1() + (accelx.get_g1() * ds));
-		velocity.set_g2(velocity.get_g2() + (accely.get_g2() * ds));
-		velocity.set_g3(velocity.get_g3() + (accelz.get_g3() * ds));
+		std::cout << "~Lx * accelx * Lx : " << accelx.toString() << std::endl;
+		std::cout << "~Ly * accely * Ly : " << accely.toString() << std::endl;
+		std::cout << "~Lz * accelz * Lz : " << accelz.toString() << std::endl;
+	
+
+		mv tempvel = velocity; //velocity.get_g1() * g1 + velocity.get_g2() * g2 + velocity.get_g3() * g3;
+		mv tempaccel = accelx + accely + accelz; //accelx.get_g1() * g1 + accely.get_g2() * g2 + accelz.get_g3() * g3;
+		//tempaccel *= ds;
+
+		//tempvel = tempvel * g0;
+		tempaccel = tempaccel * g0;
+		std::cout << "tempvel           : " << tempvel.toString() << std::endl;
+		std::cout << "tempaccel         : " << tempaccel.toString() << std::endl;
+
+		//mv finalvel = div(tempvel + tempaccel, 1 + (m4sta::sp(tempvel, tempaccel)));
+		mv finalvel = (tempvel + tempaccel) * versorInverse(1 + tempvel * tempaccel);
+		//finalvel = finalvel;
+		finalvel = g0 * finalvel;
+
+		std::cout << "finalvel          : " << finalvel.toString() << std::endl;
+		std::cout << std::endl;
+
+		
+		//velocity.set_g1(velocity.get_g1() + finalvel.get_g1());
+		//velocity.set_g2(velocity.get_g2() + finalvel.get_g2());
+		//velocity.set_g3(velocity.get_g3() + finalvel.get_g3());
+		
+		// need a way to make relativistic velocity addition work!
+		// it seems this doesn't work much - it allows to go faster
+		// than light!
+		velocity.set_g1(tempvel.get_g1() + (accelx.get_g1()));
+		velocity.set_g2(tempvel.get_g2() + (accely.get_g2()));
+		velocity.set_g3(tempvel.get_g3() + (accelz.get_g3()));
 		
 		velocity.set_g0(std::sqrt(1.0
 			+ velocity.get_g1() * velocity.get_g1()
