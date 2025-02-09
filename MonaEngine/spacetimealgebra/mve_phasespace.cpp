5a1af90e7e35c846bd6d6a4db7859bf77369988f
monapink <91607167+antfightclub@users.noreply.github.com>
2025-02-09 13:35:40 +0100

    get rid of all the stuff that didn't work

--- MonaEngine/spacetimealgebra/mve_phasespace.cpp
+++ MonaEngine/spacetimealgebra/mve_phasespace.cpp
@@ -5,77 +5,31 @@
 
 namespace mve {
 	void MvePhaseSpace::transform(m4sta::mv accel, double ds) {
+		// The transform method assumes delta time is given as proper time 
+		// meaning the time that is passing for the observer
+		// The acceleration is given as a direction of a magnitude specified by the entity
+		// for the player, it is "0.2", and this is additionally multiplied by deltatime
+
+
 		m4sta::mv temp = position;//position.get_g0() * g0 + position.get_g1() * g1 + position.get_g2() * g2 + position.get_g3() * g3;
 		position.set_g0(temp.get_g0() + velocity.get_g0() * ds);
 		position.set_g1(temp.get_g1() + velocity.get_g1() * ds);
 		position.set_g2(temp.get_g2() + velocity.get_g2() * ds);
 		position.set_g3(temp.get_g3() + velocity.get_g3() * ds);
-		
-		//std::cout << "accel   : " << accel.toString() << std::endl << std::endl;
-		//accel.set_g0(0.0);
 
-		// Constructing the inverse lorentz transformation to get back to background frame
-		double betax = -velocity.get_g1();
-		double betaz = -velocity.get_g2();
-		double betay = -velocity.get_g3(); 
+		double lorentzFactor = velocity.get_g0(); // Already done!
 
-		mv Bhat10 = g1 * g0;
-		mv Bhat20 = g2 * g0;
-		mv Bhat30 = g3 * g0;
+		mv tempvel = velocity;
+		mv tempaccel = accel;
 
-		double rapidityx = 0.5 * (log((1.0 + betax) / (1.0 - betax)));
-		double rapidityy = 0.5 * (log((1.0 + betay) / (1.0 - betay)));
-		double rapidityz = 0.5 * (log((1.0 + betaz) / (1.0 - betaz)));
+		double invLorentzFactor = 1.0 / lorentzFactor;
 
-		mv Lx = exp(0.5 * rapidityx * Bhat10);
-		mv Ly = exp(0.5 * rapidityy * Bhat20);
-		mv Lz = exp(0.5 * rapidityz * Bhat30);
+		tempaccel.set_g0(invLorentzFactor * (
+			tempvel.get_g1() * tempaccel.get_g1()
+			+ tempvel.get_g2() * tempaccel.get_g2()
+			+ tempvel.get_g3() * tempaccel.get_g3()));
 
-		mv accelx = accel.get_g1() * g1;
-		mv accely = accel.get_g2() * g2;
-		mv accelz = accel.get_g3() * g3;
-
-		accelx *= ds;
-		accely *= ds;
-		accelz *= ds;
-
-		std::cout << "betax             : " << betax << std::endl;
-		std::cout << "betay             : " << betay << std::endl;
-		std::cout << "betaz             : " << betaz << std::endl;
-
-		std::cout << "rapidityx         : " << rapidityx << std::endl;
-		std::cout << "rapidityy         : " << rapidityy << std::endl;
-		std::cout << "rapidityz         : " << rapidityz << std::endl;
-
-		std::cout << "Lx                : " << Lx.toString() << std::endl;
-		std::cout << "Ly                : " << Ly.toString() << std::endl;
-		std::cout << "Lz                : " << Lz.toString() << std::endl;
-
-		accelx = (~Lx) * accelx * Lx;
-		accely = (~Ly) * accely * Ly;
-		accelz = (~Lz) * accelz * Lz;
-
-		std::cout << "~Lx * accelx * Lx : " << accelx.toString() << std::endl;
-		std::cout << "~Ly * accely * Ly : " << accely.toString() << std::endl;
-		std::cout << "~Lz * accelz * Lz : " << accelz.toString() << std::endl;
-	
-
-		mv tempvel = velocity; //velocity.get_g1() * g1 + velocity.get_g2() * g2 + velocity.get_g3() * g3;
-		mv tempaccel = accelx + accely + accelz; //accelx.get_g1() * g1 + accely.get_g2() * g2 + accelz.get_g3() * g3;
-		//tempaccel *= ds;
-
-		//tempvel = tempvel * g0;
-		tempaccel = tempaccel * g0;
-		std::cout << "tempvel           : " << tempvel.toString() << std::endl;
-		std::cout << "tempaccel         : " << tempaccel.toString() << std::endl;
-
-		//mv finalvel = div(tempvel + tempaccel, 1 + (m4sta::sp(tempvel, tempaccel)));
-		mv finalvel = (tempvel + tempaccel) * versorInverse(1 + tempvel * tempaccel);
-		//finalvel = finalvel;
-		finalvel = g0 * finalvel;
-
-		std::cout << "finalvel          : " << finalvel.toString() << std::endl;
-		std::cout << std::endl;
+		std::cout << "acceleration = " << tempaccel.toString() << std::endl;
 
 		
 		//velocity.set_g1(velocity.get_g1() + finalvel.get_g1());
@@ -85,9 +39,9 @@ namespace mve {
 		// need a way to make relativistic velocity addition work!
 		// it seems this doesn't work much - it allows to go faster
 		// than light!
-		velocity.set_g1(tempvel.get_g1() + (accelx.get_g1()));
-		velocity.set_g2(tempvel.get_g2() + (accely.get_g2()));
-		velocity.set_g3(tempvel.get_g3() + (accelz.get_g3()));
+		velocity.set_g1(tempvel.get_g1() + (tempaccel.get_g1() * ds));
+		velocity.set_g2(tempvel.get_g2() + (tempaccel.get_g2() * ds));
+		velocity.set_g3(tempvel.get_g3() + (tempaccel.get_g3() * ds));
 		
 		velocity.set_g0(std::sqrt(1.0
 			+ velocity.get_g1() * velocity.get_g1()
